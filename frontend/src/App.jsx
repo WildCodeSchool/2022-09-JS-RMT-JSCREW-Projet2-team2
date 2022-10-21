e4d5140aaa53d4baa4034710ccb6b9bc1d4a1d51
BarzaGuignol <yayav.yaya@gmail.com>
2022-10-21 11:01:58 +0200

    ajout navbar et footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,22 @@
 import Home from "@pages/Home";
 import OneProduct from "@pages/OneProduct";
-import pokemons from "@services/data";
 import AllProduct from "@pages/AllProduct";
+
+import Navbar from "@components/Navbar";
+import Footer from "@components/Footer";
+
+import pokemons from "@services/data";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Home pokemons={pokemons} />
       <AllProduct />
       <OneProduct />
+      <Footer />
     </div>
   );
 }
