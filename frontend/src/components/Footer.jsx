c918716e673a8b3c8617ff6992d8bfd1fa00da75
PETACCIA <petaccia.seb@gmail.com>
2022-10-26 18:04:57 +0200

    fix frontend

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,7 +1,46 @@
 import React from "react";
+import "./footer.css";
 
 function Footer() {
-  return <div>Footer</div>;
-}
+  return (
+    <div className="contact">
+      <div className="container-fluid">
+        <form className=" form ">
+          <div className="container-input ">
+            <input
+              className="input-name"
+              type="text"
+              name="name"
+              placeholder="Name"
+            />
+          </div>
 
+          <div className="container-input">
+            <input
+              className="input-email"
+              type="email"
+              name="email"
+              placeholder="Email"
+            />
+          </div>
+
+          <div className=" container-textarea">
+            <textarea
+              className="textarea"
+              cols="23"
+              placeholder="Enter your message"
+              rows="3"
+            />
+          </div>
+
+          <div className=" container-button">
+            <button className="button" type="submit">
+              submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
 export default Footer;
