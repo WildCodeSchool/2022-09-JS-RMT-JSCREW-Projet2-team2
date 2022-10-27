1561924497cf25c03f163de85c325f4484aa7d32
o6ris <loic.tsiry@gmail.com>
2022-10-27 18:57:52 +0200

    retrait de la logique de naviguation vers la page produit car pas prévu dans ma US

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -20,11 +20,7 @@ function App() {
       {/* Si la valeur de page = AllProduct alors on est sur la page de vente */}
       {page === "AllProduct" ? <AllProduct setPage={setPage} /> : ""}
       {/* On filtre les pokemons, et quand la valeur de page = ID d'un pokemon on se trouve sur la page correspondante */}
-      {pokemons
-        .filter((pokemon) => page === pokemon.pokedex_index)
-        .map((pokemon, index) => (
-          <OneProduct pokemon={pokemon} index={index} />
-        ))}
+      <OneProduct />
       <Footer />
     </div>
   );
