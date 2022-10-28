0071b5d7717deb73df74ffed8db8644b25aacb7f
BarzaGuignol <yayav.yaya@gmail.com>
2022-10-28 12:14:56 +0200

    modif affichage app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,4 @@
 import Home from "@pages/Home";
-import OneProduct from "@pages/OneProduct";
-import AllProduct from "@pages/AllProduct";
 
 import Navbar from "@components/Navbar";
 import Footer from "@components/Footer";
@@ -14,8 +12,6 @@ function App() {
     <div className="App">
       <Navbar />
       <Home pokemons={pokemons} />
-      <AllProduct />
-      <OneProduct />
       <Footer />
     </div>
   );
