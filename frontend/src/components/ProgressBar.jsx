ce996a4bbef737bde6ada38d922b412defb17223
Chrisdu44 <christopherguichard44@gmail.com>
2022-11-03 10:56:30 +0100

    finalisation carte + css, création composant progressbar

---
+++ frontend/src/components/ProgressBar.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+function ProgressBar() {
+  return (
+    <div className="progressBarOneProduct">
+        <h1 className="text-white m-0 mt-2 mb-1 fs-6">Attack</h1>
+        <div className="progress" style={{ width: 100 * 2 }}>
+          <div
+            className="progress-bar progress-bar-striped progress-bar-animated bg-warning"
+            style={{ width: 49 * 2 }}
+          ></div>
+        </div>
+        <h1 className="text-white m-0 mt-2 mb-1 fs-6">Defense</h1>
+        <div className="progress" style={{ width: 100 * 2 }}>
+          <div
+            className="progress-bar progress-bar-striped progress-bar-animated bg-warning"
+            style={{ width: 49 * 2 }}
+          ></div>
+        </div>
+        <h1 className="text-white m-0 mt-2  mb-1 fs-6">Special Attack</h1>
+        <div className="progress" style={{ width: 100 * 2 }}>
+          <div
+            className="progress-bar progress-bar-striped progress-bar-animated bg-warning"
+            style={{ width: 65 * 2 }}
+          ></div>
+        </div>
+        <h1 className="text-white m-0 mt-2  mb-1 fs-6">Special Defense</h1>
+        <div className="progress" style={{ width: 100 * 2 }}>
+          <div
+            className="progress-bar progress-bar-striped progress-bar-animated bg-warning"
+            style={{ width: 65 * 2 }}
+          ></div>
+        </div>
+        <h1 className="text-white m-0 mt-2  mb-1 fs-6">Speed</h1>
+        <div className="progress" style={{ width: 100 * 2 }}>
+          <div
+            className="progress-bar progress-bar-striped progress-bar-animated bg-warning"
+            style={{ width: 45 * 2 }}
+          ></div>
+        </div>
+      </div>
+  )
+}
+
+export default ProgressBar
--- frontend/src/components/TemplateCard.jsx
+++ frontend/src/components/TemplateCard.jsx
@@ -3,23 +3,72 @@ import "./TemplateCard.css";
 
 function TemplateCard() {
   return (
-    <div className="container ">
-      <div className="card">
-        <div className="card-inner">
-          <div className="card-front">
-            <div className="card-content">
-              <h2>WawaSensei</h2>
-              <p>Tutos dev</p>
-              <p>HTML, CSS, JS</p>
+    <div className="container d-flex flex-column align-items-center justify-content-around text-dark pt-2">
+      <div className="card-info rounded-3 p-2">
+        <div className="card-front align-items-center bg-success w-100 h-100 p-3 rounded-3">
+          <div className="container-card d-grid w-100">
+            <div className="top-of-card d-flex justify-content-between align-items-center fw-bold">
+              <p>Bulbasaur</p>
+              <p>45Hp</p>
+            </div>
+            <div className="picture-card-oneProduct bg-light"></div>
+            <div className="container-attack-card d-flex flex-column justify-content-around">
+              <div className="attack-card d-flex justify-content-between align-items-center p-2 mt-2 rounded ">
+                <p className="move-attack justify-content-center mb-0 fw-bold">
+                  1st move
+                </p>
+                <p className="move-attack justify-content-center mb-0 fw-bold">
+                  Razord Wind
+                </p>
+              </div>
+              <div className="attack-card d-flex justify-content-between align-items-center p-2 mt-2 rounded">
+                <p className="move-attack justify-content-center mb-0 fw-bold">
+                  2nd move
+                </p>
+                <p className="move-attack justify-content-center mb-0 fw-bold">
+                  Swords Dance
+                </p>
+              </div>
+              <div className="attack-card d-flex justify-content-between align-items-center p-2 mt-2 rounded">
+                <p className="move-attack justify-content-center mb-0 fw-bold">
+                  3rd move
+                </p>
+                <p className="move-attack justify-content-center mb-0 fw-bold">
+                  Cut
+                </p>
+              </div>
+              <div className="attack-card d-flex justify-content-between align-items-center p-2 mt-2 rounded">
+                <p className="move-attack justify-content-center mb-0 fw-bold">
+                  4th move
+                </p>
+                <p className="move-attack justify-content-center mb-0 fw-bold">
+                  Blind
+                </p>
+              </div>
             </div>
           </div>
-          <div className="card-back">
-            <div className="card-top">
-              <div className="card-image" />
-            </div>
+          <div className="bottom-of-card d-flex align-items-center container-fluid mt-3 bg-light">
+            <p className="p-bottom m-auto">Def:49</p>
+            <p className="p-bottom m-auto">S.Def:65</p>
+            <p className="p-bottom m-auto">Att:49</p>
+            <p className="p-bottom m-auto">S.Att:65</p>
+            <p className="p-bottom m-auto">Speed:45</p>
+            <p className="p-bottom m-auto">Exp:64</p>
+          </div>
+        </div>
+        <div className="card-back">
+          <div className="card-top">
+            <div className="card-image" />
           </div>
         </div>
       </div>
+      <button
+        className="buttonColor mt-4 mb-2"
+        type="button"
+        class="btn btn-warning mt-2"
+      >
+        Add Card
+      </button>
     </div>
   );
 }
