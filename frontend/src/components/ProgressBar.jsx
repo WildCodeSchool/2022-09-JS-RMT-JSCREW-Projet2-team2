82b9c6e03ae779408c084a579fe1f8912c487690
Chrisdu44 <christopherguichard44@gmail.com>
2022-11-03 14:59:22 +0100

    modification des h1 en h3 sur progressbar.jsx

--- frontend/src/components/ProgressBar.jsx
+++ frontend/src/components/ProgressBar.jsx
@@ -4,35 +4,35 @@ import "./ProgressBar.css";
 function ProgressBar() {
   return (
     <div className="progressBarOneProduct">
-      <h1 className="text-white m-0 mt-2 mb-1 fs-6">Attack</h1>
+      <h3 className="text-white m-0 mt-2 mb-1 fs-6">Attack</h3>
       <div className="progress" style={{ width: 100 * 2 }}>
         <div
           className="progress-bar progress-bar-striped progress-bar-animated bg-warning"
           style={{ width: 49 * 2 }}
         />
       </div>
-      <h1 className="text-white m-0 mt-2 mb-1 fs-6">Defense</h1>
+      <h3 className="text-white m-0 mt-2 mb-1 fs-6">Defense</h3>
       <div className="progress" style={{ width: 100 * 2 }}>
         <div
           className="progress-bar progress-bar-striped progress-bar-animated bg-warning"
           style={{ width: 49 * 2 }}
         />
       </div>
-      <h1 className="text-white m-0 mt-2  mb-1 fs-6">Special Attack</h1>
+      <h3 className="text-white m-0 mt-2  mb-1 fs-6">Special Attack</h3>
       <div className="progress" style={{ width: 100 * 2 }}>
         <div
           className="progress-bar progress-bar-striped progress-bar-animated bg-warning"
           style={{ width: 65 * 2 }}
         />
       </div>
-      <h1 className="text-white m-0 mt-2  mb-1 fs-6">Special Defense</h1>
+      <h3 className="text-white m-0 mt-2  mb-1 fs-6">Special Defense</h3>
       <div className="progress" style={{ width: 100 * 2 }}>
         <div
           className="progress-bar progress-bar-striped progress-bar-animated bg-warning"
           style={{ width: 65 * 2 }}
         />
       </div>
-      <h1 className="text-white m-0 mt-2  mb-1 fs-6">Speed</h1>
+      <h3 className="text-white m-0 mt-2  mb-1 fs-6">Speed</h3>
       <div className="progress" style={{ width: 100 * 2 }}>
         <div
           className="progress-bar progress-bar-striped progress-bar-animated bg-warning"
