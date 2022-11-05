5613b8c6c1a048b99bb987cd60ff219ee5ca16f7
o6ris <loic.tsiry@gmail.com>
2022-11-05 08:42:55 +0100

    add style to the button

--- frontend/src/components/AddQuantity.jsx
+++ frontend/src/components/AddQuantity.jsx
@@ -1,17 +1,18 @@
 import React from "react";
+import "./addQuantity.css";
 
 function AddQuantity() {
   return (
-    <div className="d-flex justify-content-around border rounded-3 w-25 p-2">
-      <button className="bg-transparent" type="button">
+    <div className="addQuantityButton d-flex justify-content-around rounded-3 w-25 px-2 py-1">
+      <button className="bg-transparent text-white" type="button">
         -
       </button>
       <input
         type="text"
-        className="bg-transparent text-center w-25"
+        className="bg-transparent text-white text-center w-25"
         value="1"
       />
-      <button className="bg-transparent" type="button">
+      <button className="bg-transparent text-white" type="button">
         +
       </button>
     </div>
