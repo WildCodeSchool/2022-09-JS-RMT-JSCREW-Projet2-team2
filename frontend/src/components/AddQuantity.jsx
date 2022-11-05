453e6faa6c52f4f250b9b78c1ca34baec36b739e
o6ris <loic.tsiry@gmail.com>
2022-11-05 09:17:17 +0100

    ajout d'une fonction pour gérer la quantité au click sur plus ou moins

--- frontend/src/components/AddQuantity.jsx
+++ frontend/src/components/AddQuantity.jsx
@@ -3,9 +3,23 @@ import "./addQuantity.css";
 
 function AddQuantity() {
   const [quantity, setQuantity] = useState(1);
+  const handleQuantityOnClick = (operator) => {
+    if (operator === "plus") {
+      setQuantity(quantity + 1);
+    }
+    if (operator === "minus") {
+      if (quantity > 1) {
+        setQuantity(quantity - 1);
+      }
+    }
+  };
   return (
     <div className="addQuantityButton d-flex justify-content-around rounded-3 w-25 px-2 py-1">
-      <button className="bg-transparent text-white" type="button">
+      <button
+        onClick={() => handleQuantityOnClick("minus")}
+        className="bg-transparent text-white"
+        type="button"
+      >
         -
       </button>
       <input
@@ -14,7 +28,11 @@ function AddQuantity() {
         className="bg-transparent text-white text-center w-100"
         value={quantity}
       />
-      <button className="bg-transparent text-white" type="button">
+      <button
+        onClick={() => handleQuantityOnClick("plus")}
+        className="bg-transparent text-white"
+        type="button"
+      >
         +
       </button>
     </div>
