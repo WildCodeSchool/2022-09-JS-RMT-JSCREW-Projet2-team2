4838546ead48d7205cf6e7c8183d3a4c5a5ffb8b
o6ris <loic.tsiry@gmail.com>
2022-11-05 20:13:14 +0100

    ajout du bouton add favorite et création de sa classe css

---
+++ frontend/src/components/AddFavorite.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./addFavorite.css";
+
+function AddFavorite() {
+  return <div className="addFavoriteButton" />;
+}
+
+export default AddFavorite;
--- frontend/src/pages/OneProduct.jsx
+++ frontend/src/pages/OneProduct.jsx
@@ -1,11 +1,13 @@
+import AddFavorite from "@components/AddFavorite";
 import React from "react";
 import TemplateCard from "../components/TemplateCard";
 
-function OneProduct() {
+function OneProduct({ pokemon }) {
   return (
     <div className="container d-flex flex-column align-items-center justify-content-around text-dark pt-2 bg-container">
       <h1>Product</h1>
-      <TemplateCard />
+      <TemplateCard pokemon={pokemon} />
+      <AddFavorite />
     </div>
   );
 }
