d8fa8ac37e3b56821f44f643d2ada7d8a97b1744
o6ris <loic.tsiry@gmail.com>
2022-11-05 08:48:57 +0100

    ajout du state et de la methode onChange sur le input.

--- frontend/src/components/AddQuantity.jsx
+++ frontend/src/components/AddQuantity.jsx
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./addQuantity.css";
 
 function AddQuantity() {
+  const [quantity, setQuantity] = useState(1);
   return (
     <div className="addQuantityButton d-flex justify-content-around rounded-3 w-25 px-2 py-1">
       <button className="bg-transparent text-white" type="button">
         -
       </button>
       <input
+        onChange={(e) => setQuantity(e.target.value)}
         type="text"
-        className="bg-transparent text-white text-center w-25"
-        value="1"
+        className="bg-transparent text-white text-center w-100"
+        value={quantity}
       />
       <button className="bg-transparent text-white" type="button">
         +
