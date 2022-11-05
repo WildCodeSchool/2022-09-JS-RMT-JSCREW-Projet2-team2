726cf59872e0f0981c81a8164253a14c67e98ec9
o6ris <loic.tsiry@gmail.com>
2022-11-05 08:32:02 +0100

    create the template button add quantity

--- frontend/src/components/AddQuantity.jsx
+++ frontend/src/components/AddQuantity.jsx
@@ -2,12 +2,18 @@ import React from "react";
 
 function AddQuantity() {
   return (
-    <div className="d-flex justify-content-center">
-      <button type="button">-</button>
-      <div>
-        <input type="text" className="border border-0" />
-      </div>
-      <button type="button">+</button>
+    <div className="d-flex justify-content-around border rounded-3 w-25 p-2">
+      <button className="bg-transparent" type="button">
+        -
+      </button>
+      <input
+        type="text"
+        className="bg-transparent text-center w-25"
+        value="1"
+      />
+      <button className="bg-transparent" type="button">
+        +
+      </button>
     </div>
   );
 }
