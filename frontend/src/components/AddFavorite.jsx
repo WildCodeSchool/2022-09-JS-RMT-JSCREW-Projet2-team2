fcbe73e889ec36adc0335acae31d9cb5f47c3a9b
o6ris <loic.tsiry@gmail.com>
2022-11-06 11:28:42 +0100

    ajout du state et de la fonction pour sauvegarder la valeur de isFavorite et gérer le bouton isFavorite

--- frontend/src/components/AddFavorite.jsx
+++ frontend/src/components/AddFavorite.jsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./addFavorite.css";
 
 function AddFavorite() {
-  return <div className="addFavoriteButton" />;
+  const [isFavorite, setIsFavorite] = useState(false);
+  // fonction qui gère le passage du bouton en favoris.
+  const handleIsFavorite = () => {
+    setIsFavorite(!isFavorite);
+  };
+
+  return (
+    <button
+      onClick={handleIsFavorite}
+      // Toggle de class en fcontion de la valeur de isFavorite (pour le changement visuel du bouton)
+      className={isFavorite ? "isFavorite" : "notFavorite"}
+      type="button"
+      aria-label="add to favorite"
+    />
+  );
 }
 
 export default AddFavorite;
