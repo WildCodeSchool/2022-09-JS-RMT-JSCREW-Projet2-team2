e92b80b0ef6f805dd3798887f6e6c474cb496d5e
kadev <karimaoudiaa@gmail.com>
2022-11-07 11:16:25 +0100

    ajout route avec param id

--- backend/src/router.js
+++ backend/src/router.js
@@ -8,4 +8,16 @@ router.get("/pokebiz", (req, res) => {
   res.json(pokemons);
 });
 
+const getPokemonsId = (req, res) => {
+  const poke = pokemons.find(
+    (pokemon) => pokemon.pokedex_index === parseInt(req.params.id, 10)
+  );
+  if (poke !== null) {
+    res.json(poke);
+  } else {
+    res.status(404).send("no found");
+  }
+};
+router.get("/pokebiz/:id", getPokemonsId);
+
 module.exports = router;
