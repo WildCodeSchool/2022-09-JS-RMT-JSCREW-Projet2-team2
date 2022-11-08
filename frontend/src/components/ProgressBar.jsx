2be8913e3296aeccee938ae181436677bf8b8e85
o6ris <loic.tsiry@gmail.com>
2022-11-08 18:40:34 +0100

    création du composant TemplateProgressBar, appel dans ProgressBar et passage des props

--- frontend/src/components/ProgressBar.jsx
+++ frontend/src/components/ProgressBar.jsx
@@ -1,73 +1,21 @@
 import React from "react";
+import TemplateProgressBar from "./TemplateProgressBar";
 import "./ProgressBar.css";
 
 function ProgressBar({ pokemon }) {
   return (
     <div className="progressBarOneProduct">
-      <div className="d-flex flex-column align-items-start w-100">
-        <h3 className="text-white ms-2 mt-2 mb-1 fs-6">Attack</h3>
-        <div className="d-flex w-100 align-items-center justify-content-between">
-          <div className="progress" style={{ width: 100 * 2.8 }}>
-            <div
-              className="progress-bar progress-bar-striped progress-bar-animated"
-              style={{ width: pokemon.stats.attack * 2.8 }}
-            />
-          </div>
-          <h4 className="mb-0 text-white fs-5">{pokemon.stats.attack}</h4>
-        </div>
-      </div>
-      <div className="d-flex flex-column align-items-start">
-        <h3 className="text-white ms-2 mt-3 mb-1 fs-6">Defense</h3>
-        <div className="d-flex w-100 align-items-center justify-content-between">
-          <div className="progress" style={{ width: 100 * 2.8 }}>
-            <div
-              className="progress-bar progress-bar-striped progress-bar-animated"
-              style={{ width: pokemon.stats.defense * 2.8 }}
-            />
-          </div>
-          <h4 className="mb-0 text-white fs-5">{pokemon.stats.defense}</h4>
-        </div>
-      </div>
-      <div className="d-flex flex-column align-items-start">
-        <h3 className="text-white ms-2 mt-3  mb-1 fs-6">Special Attack</h3>
-        <div className="d-flex w-100 align-items-center justify-content-between">
-          <div className="progress" style={{ width: 100 * 2.8 }}>
-            <div
-              className="progress-bar progress-bar-striped progress-bar-animated"
-              style={{ width: pokemon.stats.special_attack * 2.8 }}
-            />
-          </div>
-          <h4 className="mb-0 text-white fs-5">
-            {pokemon.stats.special_attack}
-          </h4>
-        </div>
-      </div>
-      <div className="d-flex flex-column align-items-start">
-        <h3 className="text-white ms-2 mt-3  mb-1 fs-6">Special Defense</h3>
-        <div className="d-flex w-100 align-items-center justify-content-between">
-          <div className="progress" style={{ width: 100 * 2.8 }}>
-            <div
-              className="progress-bar progress-bar-striped progress-bar-animated"
-              style={{ width: pokemon.stats.special_defence * 2.8 }}
-            />
-          </div>
-          <h4 className="mb-0 text-white fs-5">
-            {pokemon.stats.special_defence}
-          </h4>
-        </div>
-      </div>
-      <div className="d-flex flex-column align-items-start">
-        <h3 className="text-white ms-2 mt-3  mb-1 fs-6">Speed</h3>
-        <div className="d-flex w-100 align-items-center justify-content-between">
-          <div className="progress" style={{ width: 100 * 2.8 }}>
-            <div
-              className="progress-bar progress-bar-striped progress-bar-animated"
-              style={{ width: pokemon.stats.speed * 2.8 }}
-            />
-          </div>
-          <h4 className="mb-0 text-white fs-5">{pokemon.stats.speed}</h4>
-        </div>
-      </div>
+      <TemplateProgressBar stats={pokemon.stats.attack} statName="attack" />
+      <TemplateProgressBar stats={pokemon.stats.defense} statName="defense" />
+      <TemplateProgressBar
+        stats={pokemon.stats.special_attack}
+        statName="special attack"
+      />
+      <TemplateProgressBar
+        stats={pokemon.stats.special_defence}
+        statName="special defense"
+      />
+      <TemplateProgressBar stats={pokemon.stats.speed} statName="speed" />
     </div>
   );
 }
