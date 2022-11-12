736fb33cb5f9b77e6ce598eda16e904367d06946
o6ris <loic.tsiry@gmail.com>
2022-11-13 00:29:42 +0100

    créationde la version desktop de oneProduct

--- frontend/src/pages/OneProduct.jsx
+++ frontend/src/pages/OneProduct.jsx
@@ -12,28 +12,63 @@ function OneProduct({ pokemon }) {
   // Création du state pour enregistrer la quantité souaihtée.
   const [quantity, setQuantity] = useState(1);
   return (
-    <div className="oneProductPage container-fluid d-flex flex-column align-items-center justify-content-around text-dark pt-5 bg-container">
-      <h1 className="text-white pb-2 text-capitalize">{pokemon.name} card</h1>
-      <div className="allproduct-card-container">
-        <TemplateCard pokemon={pokemon} />
-      </div>
-      <div className="mt-3 d-flex">
-        <AddFavorite />
-      </div>
-      <div>
-        <ProgressBar pokemon={pokemon} />
-      </div>
-      <div className="oneProduct-postCard-element my-5 d-flex align-items-center justify-content-around">
-        <div className="pokemonPrice">
-          {/* Mettre le prix en dynamique avec les props reçues */}
-          <h3 className="priceStyle">{pokemon.price} £</h3>
+    <div>
+      {/* ONE PRODUCT MOBILE VERSION */}
+      <div className="oneProductPage d-lg-none container-fluid d-flex flex-column align-items-center justify-content-around text-dark pt-5 bg-container">
+        <h1 className="text-white pb-2 text-capitalize">{pokemon.name} card</h1>
+        <div className="allproduct-card-container">
+          <TemplateCard pokemon={pokemon} />
         </div>
-        <div className="addQuantityButton">
-          {/* Envoie du state dans le composant */}
-          <AddQuantity quantity={quantity} setQuantity={setQuantity} />
+        <div className="mt-3 d-flex">
+          <AddFavorite />
         </div>
-        <div className="addToCartButton">
-          <AddToCartButton />
+        <div>
+          <ProgressBar pokemon={pokemon} />
+        </div>
+        <div className="oneProduct-postCard-element my-5 d-flex align-items-center justify-content-around">
+          <div className="pokemonPrice">
+            {/* Mettre le prix en dynamique avec les props reçues */}
+            <h3 className="priceStyle">{pokemon.price} £</h3>
+          </div>
+          <div className="addQuantityButton">
+            {/* Envoie du state dans le composant */}
+            <AddQuantity quantity={quantity} setQuantity={setQuantity} />
+          </div>
+          <div className="addToCartButton">
+            <AddToCartButton />
+          </div>
+        </div>
+      </div>
+      {/* ONE PRODUCT DESKTOP VERSION */}
+      <div className="oneProductPage d-none d-lg-flex container-fluid flex-column align-items-center justify-content-around text-dark pt-5 bg-container">
+        <h1 className="text-white pb-2 text-capitalize">{pokemon.name} card</h1>
+        <div className="d-flex mb-5">
+          {/* TEMPLATE CARD */}
+          <div className="allproduct-card-container">
+            <TemplateCard pokemon={pokemon} />
+          </div>
+          {/* STATS + ADDFAVORITE + PRICE/QUANTITY/ADDtoCART */}
+          <div className="ms-5">
+            <div className="oneProduct-postCard-element my-5 d-flex align-items-center justify-content-around">
+              <div className="pokemonPrice">
+                {/* Mettre le prix en dynamique avec les props reçues */}
+                <h3 className="priceStyle">{pokemon.price} £</h3>
+              </div>
+              <div className="addQuantityButton">
+                {/* Envoie du state dans le composant */}
+                <AddQuantity quantity={quantity} setQuantity={setQuantity} />
+              </div>
+              <div className="addToCartButton">
+                <AddToCartButton />
+              </div>
+            </div>
+            <div>
+              <ProgressBar pokemon={pokemon} />
+            </div>
+            <div className="mt-3 d-flex">
+              <AddFavorite />
+            </div>
+          </div>
         </div>
       </div>
     </div>
