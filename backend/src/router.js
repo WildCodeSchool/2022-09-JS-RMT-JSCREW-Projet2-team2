d799d5c2e3c223782c9d902c4edcba39e8170543
Chrisdu44 <christopherguichard44@gmail.com>
2022-11-16 16:20:02 +0100

    ajout des routes back AllProduct + OneProduct

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,11 +1,38 @@
 const express = require("express");
 
+const { connection } = require("./db");
+
 const router = express.Router();
 
 const pokemons = require("./data/pokemons");
 
 router.get("/pokebiz", (req, res) => {
   res.json(pokemons);
+});
+
+router.get("/AllProducts", (req, res) => {
+  connection
+    .query("SELECT * FROM pokemon")
+    .then(([response]) => {
+      res.status(200).send(response);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+});
+
+router.get("/AllProducts/:id", (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  connection
+    .query("SELECT * FROM pokemon WHERE id = ?", [id])
+    .then(([response]) => {
+      res.status(200).send(response);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
 });
 
 const getPokemonsId = (req, res) => {
